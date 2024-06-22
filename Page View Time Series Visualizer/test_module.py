3cd4a31b1467544747966b8569c1f4b661eadfb2
Hamza Laabidi <abidi.hamza000@gmail.com>
2024-06-22 17:08:32 +0100

    fourth  commit

---
+++ Page View Time Series Visualizer/test_module.py
@@ -0,0 +1,13 @@
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+
+# Draw the line plot and save it
+line_plot = draw_line_plot()
+line_plot.show()
+
+# Draw the bar plot and save it
+bar_plot = draw_bar_plot()
+bar_plot.show()
+
+# Draw the box plot and save it
+box_plot = draw_box_plot()
+box_plot.show()
